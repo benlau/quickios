2cdebb789fa7d19b5ed3b07dd01f3889f597bef7
Ben Lau <xbenlau@gmail.com>
2014-11-24 16:27:28 +0800

    Added tests/quickiosunittests - Unit tests code

---
+++ tests/quickiosunittests/quickiosunittests.pro
@@ -0,0 +1,20 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2014-11-24T16:10:59
+#
+#-------------------------------------------------
+
+QT       += testlib
+
+QT       -= gui
+
+TARGET = tst_quickiosunittests
+CONFIG   += console
+CONFIG   -= app_bundle
+
+TEMPLATE = app
+
+
+SOURCES += tst_quickiosunittests.cpp
+DEFINES += SRCDIR=\\\"$$PWD/\\\"
+include (../../quickios.pri)
---
+++ tests/quickiosunittests/tst_quickiosunittests.cpp
@@ -0,0 +1,75 @@
+#include <QString>
+#include <QtTest>
+#include <QCoreApplication>
+#include <QQmlEngine>
+#include <QQmlComponent>
+#include "quickios.h"
+
+class QuickIOSUnitTests : public QObject
+{
+    Q_OBJECT
+
+public:
+    QuickIOSUnitTests();
+
+private Q_SLOTS:
+    void initTestCase();
+    void cleanupTestCase();
+    void resourceChecking();
+};
+
+QuickIOSUnitTests::QuickIOSUnitTests()
+{
+}
+
+void QuickIOSUnitTests::initTestCase()
+{
+    QuickIOS::registerTypes();
+}
+
+void QuickIOSUnitTests::cleanupTestCase()
+{
+}
+
+void QuickIOSUnitTests::resourceChecking()
+{
+    QQueue<QString> queue;
+    queue.enqueue(":/QuickIOS");
+
+    QQmlEngine engine;
+    engine.addImportPath("qrc:///");
+
+    while (queue.size()) {
+        QString path = queue.dequeue();
+        QDir dir(path);
+        QFileInfoList infos = dir.entryInfoList(QStringList());
+        for (int i = 0 ; i < infos.size();i++) {
+            QFileInfo info = infos.at(i);
+            if (info.fileName() == "." || info.fileName() == "..")
+                continue;
+            if (info.isDir()) {
+                queue.enqueue(info.absoluteFilePath());
+                continue;
+            }
+            QUrl url = info.absoluteFilePath().remove(0,1);
+            url.setScheme("qrc");
+
+            if (info.suffix() != "qml") {
+                continue;
+            }
+
+            QQmlComponent comp(&engine);
+            comp.loadUrl(url);
+            if (comp.isError()) {
+                qDebug() << QString("%1 : Load Failed. Reason :  %2").arg(info.absoluteFilePath()).arg(comp.errorString());
+            }
+            QVERIFY(!comp.isError());
+
+            qDebug() << QString("%1 : Passed").arg(info.absoluteFilePath());
+        }
+    }
+}
+
+QTEST_MAIN(QuickIOSUnitTests)
+
+#include "tst_quickiosunittests.moc"
