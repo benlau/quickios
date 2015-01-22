f01b37ecd2a2cdeb03bbec501b9d41566b5c8b0e
Ben Lau <xbenlau@gmail.com>
2015-01-22 20:33:58 +0800

    quickiosqmlunittests : Added backtrace report on crash

--- tests/quickiosqmlunittests/main.cpp
+++ tests/quickiosqmlunittests/main.cpp
@@ -5,9 +5,27 @@
 #include <QtQml>
 #include <QtGui>
 #include <QVariantMap>
+#include <execinfo.h>
+#include <signal.h>
+#include <unistd.h>
 #include "quickios.h"
 
 int waitTime = 1000;
+
+
+void handleBacktrace(int sig) {
+  void *array[100];
+  size_t size;
+
+  // get void*'s for all entries on the stack
+  size = backtrace(array, 100);
+
+  // print out all the frames to stderr
+  fprintf(stderr, "Error: signal %d:\n", sig);
+  backtrace_symbols_fd(array, size, STDERR_FILENO);
+  exit(1);
+}
+
 
 static QJSValue envProvider(QQmlEngine *engine, QJSEngine *scriptEngine)
 {
@@ -18,6 +36,8 @@ static QJSValue envProvider(QQmlEngine *engine, QJSEngine *scriptEngine)
 
 int main(int argc, char **argv)
 {
+    signal(SIGSEGV, handleBacktrace);
+
     QApplication a(argc, argv);
     QuickIOS::registerTypes();
     qmlRegisterSingletonType("QuickIOS", 0, 1, "TestEnv", envProvider);
